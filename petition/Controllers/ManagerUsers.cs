f11eb6958aeca20473e110765acefc013544a98f
faizandevel <faizandevel@gmail.com>
2021-04-14 07:00:28 +0500

    manager Users

---
+++ petition/Controllers/ManagerUsers.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace petition.Controllers
+{
+    public class ManagerUsers : Controller
+    {
+        [HttpGet]
+        public ActionResult AssignCoordinators()
+        {
+            return View();
+        }
+        public IActionResult AssignSubCoordinators()
+        {
+            return View();
+        }
+    }
+}
