72b495998922f387bb56254047e7a2d3cf98bd06
faizandevel <faizandevel@gmail.com>
2021-04-15 06:45:54 +0500

    reports

---
+++ petition/Controllers/Reports.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace petition.Controllers
+{
+    public class Reports : Controller
+    {
+        public IActionResult NewBatchSubmission()
+        {
+            return View();
+        }
+    }
+}
