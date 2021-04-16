d1ab1b775ae68ce2ffe3a105d84fd4b5d13c9a43
kashifkhanniazi <kashifkhanniazi45@gmail.com>
2021-04-15 17:55:44 -0700

    16-2-2021

--- petition/Controllers/ManagerUsers.cs
+++ petition/Controllers/ManagerUsers.cs
@@ -98,9 +98,38 @@ namespace petition.Controllers
             }
             return View();
         }
-        public async Task<ActionResult> ManageInternalValidators()
+        public ActionResult ManageInternalValidators()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> AddKPMUser(string id, string fname,string lname)
+        {
+            var percent = await _userManager.FindByNameAsync(User.Identity.Name);
+            var strdisplayname = lname + ", " + fname;
+            var dnnUserID = id;
+            var enteredby = percent.Id;
+            var dateentered = DateTime.Now;
+            var kpmusertype = "External Validator";
+            int kpmuserid;
+            var commandText = "INSERT INTO dbo.KpmUsers(kpmusertype, firstname, lastname, dateentered, enteredby, dnn_userid, displayname) VALUES ('" + kpmusertype + "','" + fname + "','" + lname + "','" + dateentered + "','" + enteredby + "','" + dnnUserID + "','" + strdisplayname + "' )";
+                try
+            {
+                var result = context.Database.ExecuteSqlRaw(commandText);
+                try
+                {
+                    commandText = "SELECT kpmuserid FROM KPM_Users where  dnn_userid = '" + dnnUserID + "'";
+                    kpmuserid = context.Database.ExecuteSqlRaw(commandText);
+                }catch(Exception e)
+                {
+                    return BadRequest(e);
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+            return Ok(kpmuserid);
         }
         [HttpGet]
         public async Task<ActionResult> AssignCoordinators()
@@ -138,11 +167,45 @@ namespace petition.Controllers
             return View();
         }
         [HttpGet]
-        public async Task<ActionResult> GetCoordinate(string type)
+        public ActionResult GetCoordinate(string type)
         {
             var commandText = "SELECT firstname, lastname, kpmuserID, coordassigned FROM KPMUsers where kpmusertype= '"+type+"' order by lastname";
             var result = context.GetCoordinate.FromSqlRaw(commandText).ToList();
             return Ok(result);
+        }
+        public async Task<ActionResult> Getcoordinators()
+        {
+            var users = await _userManager.GetUsersInRoleAsync("External Validator");
+
+            List<UserListVM> userList = new List<UserListVM>();
+            if (users.Any())
+            {
+                foreach (var user in users)
+                {
+                    userList.Add(new UserListVM
+                    {
+                        userId = user.Id,
+                        userName = user.UserName,
+                        firstName = user.FirstName,
+                        lastName = user.LastName,
+                        address = user.Address,
+                        zipCode = user.ZipCode,
+                        state = user.State,
+                        city = user.City,
+                        phoneNumber = user.PhoneNumber,
+                        authorize = user.Authorize,
+                        email = user.Email
+                    });
+                }
+                createBatchVM data = new createBatchVM();
+                if (userList != null)
+                {
+                    data.users = userList;
+                }
+
+                return Ok(data);
+            }
+            return Ok();
         }
         public async Task<IActionResult> AssignSubCoordinators()
         {
