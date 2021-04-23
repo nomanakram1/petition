0d0e49cb0cb180dc362eae237108e67137ed6810
faizandevel <faizandevel@gmail.com>
2021-04-23 10:45:21 +0500

    coordinator

---
+++ petition/Controllers/coordinator.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace petition.Controllers
+{
+    public class Coordinator : Controller
+    {
+        public IActionResult BatchSheets()
+        {
+            return View();
+        }
+        public IActionResult ManageBatches()
+        {
+            return View();
+        }
+        public IActionResult Users()
+        {
+            return View();
+        }
+        public IActionResult CirculatorActivity()
+        {
+            return View();
+        }
+        public IActionResult MyTotals()
+        {
+            return View();
+        }
+    }
+}
